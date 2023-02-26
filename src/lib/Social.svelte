f6618c0240b70df9664894fb481f611152498ffa
belevd <bankir35@mail.ru>
2023-02-26 23:54:31 +0400

    change social component

---
+++ src/lib/Social.svelte
@@ -0,0 +1,40 @@
+<script>
+  export let link, icon
+</script>
+
+<a
+    class="social__link"
+    href={link}
+    target="_blank"
+    rel="noreferrer"
+>
+    <svelte:component class="social__icon" this={icon}/>
+</a>
+
+<style>
+  .social__link {
+    position: relative;
+    color: var(--base-weak);
+    margin-bottom: 1rem;
+    display: block;
+  }
+
+  .social__link:last-child {
+    margin-bottom: 0;
+  }
+
+  .social__link:hover {
+    color: var(--base-medium);
+  }
+
+  :global(.social__icon) {
+    width: 3em;
+    display: block;
+  }
+
+  @media (max-width: 1024px) {
+    :global(.social__icon) {
+      width: 2em;
+    }
+  }
+</style>
--- src/lib/Socials.svelte
+++ src/lib/Socials.svelte
@@ -1,16 +1,11 @@
 <script>
   import {SOCIALS} from "../config/index.ts";
-
-  const onHover = (event) => {
-    // console.log(event.currentTarget.href);
-  }
+  import Social from "./Social.svelte";
 </script>
 
 <aside class="socials">
     {#each SOCIALS as social}
-        <a on:mouseover={onHover} class="social__link" href={social.link} target="_blank" rel="noreferrer">
-            <svelte:component class="social__icon" this={social.icon} />
-        </a>
+        <Social link={social.link} icon={social.icon} />
     {/each}
 </aside>
 
@@ -21,30 +16,5 @@
       top: 50%;
       z-index: 2;
       color: var(--base-weak);
-    }
-
-    .social__link {
-      color: var(--base-weak);
-      margin-bottom: 1rem;
-      display: block;
-    }
-
-    .social__link:last-child {
-      margin-bottom: 0;
-    }
-
-    .social__link:hover {
-      color: var(--base-medium);
-    }
-
-    :global(.social__icon) {
-      width: 3em;
-      display: block;
-    }
-
-    @media (max-width: 1024px) {
-      :global(.social__icon) {
-        width: 2em;
-      }
     }
 </style>
